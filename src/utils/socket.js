4d52046a4b52f942b01a3a709219021de1651778
tamimjdd <deviltamim88@gmail.com>
2022-07-06 20:01:25 +0600

    base url bug fixed

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,7 @@
 import socketio from "socket.io-client";
 const accessToken = JSON.parse(localStorage.getItem("authToken"));
 
-export const newSocket = socketio("http://192.168.1.15:3000", {
+export const newSocket = socketio("http://officetalk.tetonelectronics.com:3000", {
   transports: ['websocket'],
   query: {
     token: accessToken
