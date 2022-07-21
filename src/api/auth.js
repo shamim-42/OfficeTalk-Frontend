6612a285982c0c51b997f731b03dafd4e67f4910
abusaid7388 <abu.said@tetonelectronics.com>
2022-07-21 08:31:07 +0600

    change url port

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,51 +2,51 @@ import connector from "../connector";
 
 
 export const userRegistrationApi = async function (payload, options = {}) {
-  const url = '4001/auth/signup';
+  const url = '4000/auth/signup';
   return await connector.post(url, payload, options);
 };
 
 export const userLoginApi = async function (payload, options = {}) {
-  const url = '4001/auth/signin';
+  const url = '4000/auth/signin';
   return await connector.post(url, payload, options);
 };
 
 export const userLogoutApi = async function (id, options = {}) {
-  const url = `3001/chat/${id}/signout`;
+  const url = `3000/chat/${id}/signout`;
   return await connector.get(url, options);
 };
 
 export const editprofileApi = async function (id, payload, options = {}) {
-  const url = `4001/users/edit/${id}`;
+  const url = `4000/users/edit/${id}`;
   return await connector.put(url, payload, options);
 }
 
 export const editprofilePhotoApi = async function (id, payload, options = {}) {
-  const url = `4001/upload/${id}`;
+  const url = `4000/upload/${id}`;
   return await connector.post(url, payload, options);
 }
 
 export const editPasswordApi = async function (id, payload, options = {}) {
-  const url = `4001/users/${id}/changepassword`;
+  const url = `4000/users/${id}/changepassword`;
   return await connector.put(url, payload, options);
 }
 
 export const allUserListApi = async function (options = {}) {
-  const url = '4001/users';
+  const url = '4000/users';
   return await connector.get(url, options);
 };
 
 export const userActiveStatusApi = async function (id, options = {}) {
-  const url = `4001/users/${id}/activestatus`;
+  const url = `4000/users/${id}/activestatus`;
   return await connector.get(url, options);
 };
 
 export const friendListApi = async function (userId, options = {}) {
-  const url = `4001/auth/friendList/${userId}`;
+  const url = `4000/auth/friendList/${userId}`;
   return await connector.get(url, options);
 };
 
 export const checkJWTToken = async function (payload, options = {}) {
-  const url = '4001/auth/verifyjwt';
+  const url = '4000/auth/verifyjwt';
   return await connector.post(url, payload, options);
 }
